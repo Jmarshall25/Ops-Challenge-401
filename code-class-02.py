78d9a3b205f1fe1c8c2fe5acf0edb9b245d8e396
Jordan Marshall <121899029+Jmarshall25@users.noreply.github.com>
2023-04-19 11:11:41 -0400

    Update code-class-02.py

--- code-class-02.py
+++ code-class-02.py
@@ -7,7 +7,24 @@
 
 # Main
 
+# Import Libraries
+import os, time, datetime
 
+# Perform ping
+target = "8.8.8.8"
+response = os.system("ping -c 1" + target)
+
+# Check Host Status 
+if response == 0:
+    print("Network is up")
+else:
+    print("Network Error")
+    
+# Wait Three Seconds
+time.sleep(3)
+
+# End 
+                     
 
 
 
