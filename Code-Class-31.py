f37811ea37ba48368710835fbd852e1d3b854b3d
jmarshall25 <jordanmarshall25@yahoo.com>
2023-05-31 06:47:13 +0000

    malware

---
+++ Code-Class-31.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Script: Ops 401 Class 31 Part 1/3
+# Author: Jordan Marshall
+# Date of latest revision: 30Msy2023
+# Purpose: Malware  Detection
+# Help from class demo and ChatGPT
+
+# Main
+
+# Import Libraries
