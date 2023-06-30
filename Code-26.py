5c5e2b755a24f3e50988ff50e364af5cc9cdc513
Jordan Marshall <121899029+Jmarshall25@users.noreply.github.com>
2023-06-30 16:46:40 -0400

    logging1

---
+++ Code-26.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# Jordan Marshall
+# Attributes to class demo & Geneva Knott
+# Event Logging 
+
+# Main
+
+import logging
+import os
+import datetime
+import time
+
+target = "8.8.8.0"
+
+def ping_Status(target):
+    try:
+         #Intentionally raise an exception to simulate an error
+        # raise ValueError("An intentional error occurred")   
+        # Evaluate the response and assign success or failure to the status variable
+        icmp = os.system("ping -c 1 " + target)
+        if icmp == 0:
+            status = "success"
+            print(f"{target} is up!")
+        else:
+            status = "failure"
+            print(f"{target} is down!")
+        
+        # Get the current timestamp and print the status and timestamp
+        currenttime = datetime.datetime.now()
+        print(f"{currenttime} - Status: {status}")
+        return status
+    except Exception as e:
+        logger.exception("An error occurred")
+        raise e
+
+# Create and configure logger
+logging.basicConfig(filename="Demo.log", format='%(asctime)s %(message)s', filemode='w')
+
+# Create object
+logger = logging.getLogger()
+
+# Setting the threshold
+logger.setLevel(logging.DEBUG)
+
+logger.info("Script started: ping_Status")
+
+while True:
+    try:
+        # Transmit a single ICMP ping packet to the target
+        ping_Status(target)
+        # Wait for 2 seconds before transmitting another ping packet
+        time.sleep(2)
+    except KeyboardInterrupt:
+        # Stop the program if the user interrupts it
+        break
+    except Exception as e:
+        logger.exception("An error occurred")
+
+logger.info("Script completed: ping_Status") 
+
+# End
